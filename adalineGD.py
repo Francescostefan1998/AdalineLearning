8af345a75ee28befbe5323ee43692d3a56cf4a20
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-12-14 18:32:33 +0100

    changing example to be more practical

--- adalineGD.py
+++ adalineGD.py
@@ -27,6 +27,7 @@ class AdalineGD:
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
+        print(f'Initializing : with eta = {eta}, n_iter = {n_iter}, random_state ={random_state}')
 
     def fit(self, X, y):
         """Fit training data.
@@ -42,19 +43,31 @@ class AdalineGD:
         self: object
 
         """
-        rgen = np.random.randomState(self.random_state)
+        rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1]) # all the weights, it is a list "kind of a random list"
-        self.b_ = np.float_(0.)
+        self.b_ = np.float64(0.)
         self.losses_ = []
-
+        print('FIT METHOD')
+        print(f'weights = {self.w_}')
+        print(f'bias =  {self.b_}')
+        print(f'self losses = {self.losses_}')
         for i in range(self.n_iter):
                  net_input = self.net_input(X)
+                 print(f'net_input = {net_input}')
                  output = self.activation(net_input)
+                 print(f'output = {output}')
                  errors =(y- output)
+                 print(f'errors = {errors}')
+                 print(f'weights before = {self.w_}')
                  self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]  # 
+                 print(f'weights after = {self.w_}')
+                 print(f'weights before = {self.b_}')
                  self.b_ += self.eta * 2.0 * errors.mean()
+                 print(f'weights before = {self.b_}')
                  loss = (errors**2).mean() # the .mean() calculate the average (all valuse/n of values)
+                 print(f'loss = {loss}')
                  self.losses_.append(loss)
+                 print(f'losses = {self.losses_}')
         return self
     
     def net_input(self, X):
---
+++ useAdaline.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+from adalineGD import AdalineGD 
+from matplotlib.colors import ListedColormap
+
+
+
+# Hardcoded dataset
+X = np.array([
+    [5, 10],  # Example 1: 5 hours screen time, 10k steps = REGULAR 1
+    [7, 5],   # Example 2: 7 hours screen time, 5k steps = NOT REGULAR 0
+    [3, 12],  # Example 3: 3 hours screen time, 12k steps = REGULAR 1
+    [8, 3],   # Example 4: 8 hours screen time, 3k steps = NOT REGULAR 0
+    [6, 8],   # Example 5: 6 hours screen time, 8k steps = REGULAR 1
+    [4, 7],   # Example 6: 4 hours screen time, 7k steps = REGULAR 1
+    [9, 4],   # Example 7: 9 hours screen time, 4k steps = NOT REGULAR 0
+    [2, 11],  # Example 8: 2 hours screen time, 11k steps = REGULAR 1
+    [6, 6],   # Example 9: 6 hours screen time, 6k steps = NOT REGULAR 0
+    [5, 9]    # Example 10: 5 hours screen time, 9k steps = REGULAR 1
+])
+
+# Labels (y)
+y = np.array([1, 0, 1, 0, 1, 1, 0, 1, 0, 1])  # 1 = Regular, 0 = Not Regular
+
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize=(10, 4))
+ada1 = AdalineGD(n_iter=3, eta=0.1).fit(X,y)
+ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Mean squared error)')
+ada2 = AdalineGD(n_iter=3, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) +1), ada2.losses_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Mean squared error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
+
